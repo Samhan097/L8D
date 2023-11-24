cdba5e0cce9a246176cd3920e12f81df2e172c65
Samhan097 <48414384+Samhan097@users.noreply.github.com>
2023-11-24 16:55:08 +0530

    L8 Digital seperate as components

---
+++ src/Home/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <>
+    <div className="navbar">
+              <div className="group-307">
+                <div className="l8-d">L8D</div>
+                <div className="text-3 text">®</div>
+              </div>
+              <div className="group-74">
+                <div className="flex-row-1">
+                  <div className="place place-2 inter-medium-mine-shaft-10-5px">Home</div>
+                  <div className="services inter-medium-mine-shaft-10-5px">Services</div>
+                  <div className="work inter-medium-mine-shaft-10-5px">Work</div>
+                  <div className="about inter-medium-mine-shaft-10-5px">About</div>
+                  <div className="blog inter-medium-mine-shaft-10-5px">Blog</div>
+                  <div className="place-1 place-2 inter-medium-mine-shaft-10-5px">Contact</div>
+                </div>
+              </div>
+            </div>
+            <div className="group-307-1">
+              <div className="l8-d">L8D</div>
+              <div className="text-4 text">®</div>
+            </div>
+            </>
+  )
+}
---
+++ src/Home/NumberedBox.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function NumberedBox() {
+  return (
+    <>
+        <div className="rectangle-258"></div>
+          <div className="rectangle-259"></div>
+          <div className="rectangle-261"></div>
+          <div className="rectangle-262"></div>
+          <div className="rectangle-263"></div>
+          <div className="rectangle-264"></div>
+          <div className="rectangle-260"></div>
+          <div className="rectangle-265"></div>
+          <div className="level8-digital-1">Level8Digital</div>
+          <div className="number number-7 inter-semi-bold-mine-shaft-80-2px">01</div>
+          <div className="number-1 number-7 inter-semi-bold-mine-shaft-80-2px">02</div>
+          <div className="number-2 number-7 inter-semi-bold-mine-shaft-80-2px">03</div>
+          <div className="number-3 number-7 inter-semi-bold-mine-shaft-80-2px">04</div>
+          <div className="number-4 number-7 inter-semi-bold-mine-shaft-80-2px">05</div>
+          <div className="number-5 number-7 inter-semi-bold-mine-shaft-80-2px">06</div>
+          <div className="number-6 number-7 inter-semi-bold-mine-shaft-80-2px">07</div>
+    </>
+  )
+}
